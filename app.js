a818af3ab4f1b046696a1b13a105572cac48f22a
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-03-16 15:28:46 +0300

    EJS ekleme commit

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res)=> {
-    const blog = {
-        id:1,
-        name: "Blog Title",
-        description: "Blog description"
+//Templates Engine
+app.set('view engine', 'ejs');
 
-    }
-res.send(blog)
-})
+//Middlerwares
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
 
 const port = 3000;
 app.listen(port, () => {
