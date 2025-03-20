2ca157416da2a359005262cb4e26d4c41380a48c
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-03-21 01:18:12 +0300

    Name ve post eklendir

--- models/Post.js
+++ models/Post.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema;
 
 //Create Schema
 const PostSchema = new Schema({
+  name: String,
   title: String,
   detail: String,
+  post : String,
   dateCreated: {
     type: Date,
     default: Date.now,
