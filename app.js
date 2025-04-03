a06ddcc52fe2f0ac35f4790ee89c25c32ac34033
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-04-03 21:59:19 +0300

    MVC sonrasi

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejs = require("ejs");
-const path = require("path");
+
 const app = express();
-const Post = require("./models/Post");
+
+const postControllers = require('./controllers/postControllers');
+const pageControllers = require('./controllers/pageControllers');
 
 //Templates Engine
 app.set("view engine", "ejs");
@@ -16,49 +18,24 @@ mongoose.connect("mongodb://localhost/cleanblog-test-db");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method',{
+  methods:['POST', 'GET']
+}));
 
 //ROUTES
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", {
-    post,
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
+app.get("/", postControllers.getAllPosts);
+app.get("/posts/:id", postControllers.getPost );
+app.post("/post", postControllers.createPost);
+app.put("/posts/:id", postControllers.updatePost);
+app.delete("/posts/:id", postControllers.deletePost);
 
-app.post("/post", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/about", pageControllers.getAboutPage);
+app.get("/add_post", pageControllers.getAddPage);
+app.get("/posts/edit/:id", pageControllers.getEditPage);
 
-app.get("/posts/edit/:id", async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  res.render("edit", {
-    post,
-  });
-});
 
-app.put("/posts/:id", async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  post.name = req.body.name;
-  post.title = req.body.title;
-  post.detail = req.body.detail;
-  post.post = req.body.post;
-  post.save();
-  res.redirect(`/posts/${req.params.id}`);
-});
+
+
 
 const port = 3000;
 app.listen(port, () => {
